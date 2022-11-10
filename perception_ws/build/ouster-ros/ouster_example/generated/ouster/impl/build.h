29e508a693bb14dd796a4a98c57654cd82d0f702
lizinan <2013427105@qq.com>
2022-11-10 02:41:12 -0500

    add

--- perception_ws/build/ouster-ros/ouster_example/generated/ouster/impl/build.h
+++ perception_ws/build/ouster-ros/ouster_example/generated/ouster/impl/build.h
@@ -2,7 +2,7 @@
 
 namespace ouster {
 
-const char* const BUILD_HASH = "978002a-dirty";
+const char* const BUILD_HASH = "dffb9e1-dirty";
 
 const char* const BUILD_TYPE = "release";
 
@@ -12,6 +12,6 @@ const char* const SDK_VERSION =
     "0.5.2";
 
 const char* const SDK_VERSION_FULL =
-    "0.5.2+978002a-dirty-release";
+    "0.5.2+dffb9e1-dirty-release";
 
 }
